984692cee8901fe0d4dbda7e3ba2c1e97454e846
yuko makino <yucom2.99@gmail.com>
2022-05-19 13:56:10 +0900

    プレビュー画面に表示

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
 import { useStateWithStorage } from "../hooks/use_state_with_storage";
+import ReactMarkdown from 'react-markdown';
 
 const Header = styled.header`
     font-size: 1.5rem;
@@ -58,7 +59,9 @@ export const Editor: React.FC = () => {
                     onChange={(event) => setText(event.target.value)}
                     value={text}
                 />
-                <Preview>プレビューエリア</Preview>
+                <Preview>
+                    <ReactMarkdown>{text}</ReactMarkdown>
+                </Preview>
             </Wrapper>
         </>
     )
